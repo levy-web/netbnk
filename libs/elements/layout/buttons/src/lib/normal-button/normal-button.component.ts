5e046f1427c98736a06e93963e06d92aa48001c2
Levy <lnjiru43@gmail.com>
2023-10-27 09:24:52 +0300

    new buttons components

---
+++ libs/elements/layout/buttons/src/lib/normal-button/normal-button.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-normal-button',
+  templateUrl: './normal-button.component.html',
+  styleUrls: ['./normal-button.component.scss'],
+})
+export class NormalButtonComponent {}
---
+++ libs/elements/layout/buttons/src/lib/slider-button/slider-button.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-slider-button',
+  templateUrl: './slider-button.component.html',
+  styleUrls: ['./slider-button.component.scss'],
+})
+export class SliderButtonComponent {}
