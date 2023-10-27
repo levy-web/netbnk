197a54928f3319159b6ab1c918738c722fe42316
Levy <lnjiru43@gmail.com>
2023-10-27 09:47:15 +0300

    added button interfaces

--- libs/elements/layout/buttons/src/lib/normal-button/normal-button.component.ts
+++ libs/elements/layout/buttons/src/lib/normal-button/normal-button.component.ts
@@ -1,8 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+import { ButtonData } from 'button'
 
 @Component({
   selector: 'lib-normal-button',
   templateUrl: './normal-button.component.html',
   styleUrls: ['./normal-button.component.scss'],
 })
-export class NormalButtonComponent {}
+export class NormalButtonComponent {
+
+    /** Input property to customize button appearance and behavior */
+    @Input() buttonData!: ButtonData;
+
+    /** Output Property which emits click events when the button is clicked */
+  
+    @Output() buttonClick: EventEmitter<void> = new EventEmitter<void>();
+  
+    /** tracks whether the button is being hovered */
+    isHovered = false;
+  
+    /** triggers the buttonClick event emitter, indicating that the button has been clicked.*/
+    handleClick(): void {
+      this.buttonClick.emit();
+    }
+  
+    /** Set hovered state to true when mouse enters */
+    handleMouseEnter(): void {
+      this.isHovered = true;
+    }
+  
+    /** Set hovered state to false when mouse leaves */
+    handleMouseLeave(): void {
+      this.isHovered = false;
+    }
+
+}
